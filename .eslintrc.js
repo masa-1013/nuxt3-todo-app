b561cbb3361670d33a3f45120a9387d866ce5e22
masa-1013 <wktkmrnhrtmmshr@gmail.com>
2023-12-28 21:06:06 +0900

    eslintの導入

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
